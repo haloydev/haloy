6938797580cde97053af7e741c22d808553736d5
Andreas <ameistad@gmail.com>
2026-01-09 09:44:21 +0100

    chore: bump version to 0.1.0-beta.20

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 import "os"
 
 const (
-	Version                  = "0.1.0-beta.19"
+	Version                  = "0.1.0-beta.20"
 	HAProxyVersion           = "3.2"
 	HaloydContainerName      = "haloyd"
 	HAProxyContainerName     = "haloy-haproxy"
