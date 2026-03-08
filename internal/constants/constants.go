1e255f59d3b389981f9d50a0cb37c7fc6be3a747
Andreas <ameistad@gmail.com>
2026-03-08 22:58:21 +0100

    chore: bump version to 0.1.0-beta.40

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 import "os"
 
 const (
-	Version                  = "0.1.0-beta.39"
+	Version                  = "0.1.0-beta.40"
 	DockerNetwork            = "haloy"
 	DefaultDeploymentsToKeep = 6
 	DefaultHealthCheckPath   = "/"
